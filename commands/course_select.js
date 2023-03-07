fcdd99daa7743b6021f408a54ac7cabe12126e30
shunt4 <sphunt@umich.edu>
2023-03-07 14:42:39 -0500

    Reformatted some code

--- commands/course_select.js
+++ commands/course_select.js
@@ -13,47 +13,67 @@ module.exports = {
 		.addRoleOption(option => option.setName(`role5`).setDescription('5th role you want to add.').setRequired(false)),
 
 	async execute(interaction, client) {
-
-		const role1 = interaction.options.getRole('role1');
-		const role2 = interaction.options.getRole(`role2`);
-		const role3 = interaction.options.getRole(`role3`);
-		const role4 = interaction.options.getRole(`role4`);
-		const role5 = interaction.options.getRole(`role5`);
-		
 		const roles = [];
-		if (role1 !== null) {
-			roles.push(new ButtonBuilder()
-				.setCustomId('button1')
-				.setLabel(`${role1.name}`)
-				.setStyle(ButtonStyle.Secondary));
+		const buttons = [];
+
+		// Declare a map to associate each button with a role
+		const roleMap = new Map();
+
+		for (i = 1; i <= 5; ++i) {
+			roles.push(interaction.options.getRole(`role${i}`));
 		}
-		if (role2 !== null) {
-			roles.push(new ButtonBuilder()
-				.setCustomId('button2')
-				.setLabel(`${role2.name}`)
-				.setStyle(ButtonStyle.Secondary));
+
+		// Loop through the roles array and create a button for each role
+		for (i = 0; i < roles.length; ++i) {
+			if (roles[i] !== null) {
+				buttons.push(
+					new ButtonBuilder()
+						.setCustomId(`button${i + 1}`)
+						.setLabel(`${roles[i].name}`)
+						.setStyle(ButtonStyle.Secondary)
+				);
+			}
 		}
-		if (role3 !== null) {
-			roles.push(new ButtonBuilder()
-				.setCustomId('button3')
-				.setLabel(`${role3.name}`)
-				.setStyle(ButtonStyle.Secondary));
+
+		// Set the roleMap entries with the button ids and the roles
+		for (let i = 0; i < roles.length; ++i) {
+			roleMap.set(`button${i + 1}`, roles[i]);
 		}
-		if (role4 !== null) {
-			roles.push(new ButtonBuilder()
-				.setCustomId('button4')
-				.setLabel(`${role4.name}`)
-				.setStyle(ButtonStyle.Secondary));
-		}
-		if (role5 !== null) {
-			roles.push(new ButtonBuilder()
-				.setCustomId('button5')
-				.setLabel(`${role5.name}`)
-				.setStyle(ButtonStyle.Secondary));
-		}
+		
+
+		// if (role1 !== null) {
+		// 	roles.push(new ButtonBuilder()
+		// 		.setCustomId('button1')
+		// 		.setLabel(`${role1.name}`)
+		// 		.setStyle(ButtonStyle.Secondary));
+		// }
+		// if (role2 !== null) {
+		// 	roles.push(new ButtonBuilder()
+		// 		.setCustomId('button2')
+		// 		.setLabel(`${role2.name}`)
+		// 		.setStyle(ButtonStyle.Secondary));
+		// }
+		// if (role3 !== null) {
+		// 	roles.push(new ButtonBuilder()
+		// 		.setCustomId('button3')
+		// 		.setLabel(`${role3.name}`)
+		// 		.setStyle(ButtonStyle.Secondary));
+		// }
+		// if (role4 !== null) {
+		// 	roles.push(new ButtonBuilder()
+		// 		.setCustomId('button4')
+		// 		.setLabel(`${role4.name}`)
+		// 		.setStyle(ButtonStyle.Secondary));
+		// }
+		// if (role5 !== null) {
+		// 	roles.push(new ButtonBuilder()
+		// 		.setCustomId('button5')
+		// 		.setLabel(`${role5.name}`)
+		// 		.setStyle(ButtonStyle.Secondary));
+		// }
 
 		const buttonRow1 = new ActionRowBuilder()
-			.addComponents(roles);
+			.addComponents(buttons)
 		const embed = new EmbedBuilder()
 			.setTitle('Course Selection Tutorial')
             .setDescription('Read the steps carefully to ensure you that you get into the right class that you are registered for.')
@@ -76,59 +96,31 @@ module.exports = {
 					value: 'Verify that the channels you need access to were added to the sidebar after selecting the classes you need. You will see the left sidebar populate with channels after you click the buttons that are the classes you need.',
 				},
 			])
+
 		await interaction.reply({ embeds: [embed], components: [buttonRow1] });
 
 		const collector = await interaction.channel.createMessageComponentCollector();
-
-		collector.on('collect', async (i) => {
+			
+		collector.on('collect', async i => {
 			const member = i.member;
-			if (i.customId === 'button1') {
-				if (!member.roles.cache.has(role1.id)) {
-					member.roles.add(role1);
-					await i.reply({ content: 'Role added', ephemeral:true });
-	
-				} else {
-					member.roles.remove(role1);
-					await i.reply({ content: 'Role removed', ephemeral:true });
-				}
-			}else if (i.customId === 'button2') {
-				if (!member.roles.cache.has(role2.id)) {
-					member.roles.add(role2);
+			let id = i.customId;
+
+			// Get the role associated with the button id from the map
+			let role = roleMap.get(id);
+			if (role) {
+				if (!member.roles.cache.has(role.id)) {
+					await member.roles.add(role);
 					await i.reply({ content: 'Role added', ephemeral:true });
 				} else {
-					member.roles.remove(role2);
+					await member.roles.remove(role);
 					await i.reply({ content: 'Role removed', ephemeral:true });
 				}
-			}else if (i.customId === 'button3') {
-				if (!member.roles.cache.has(role3.id)) {
-					member.roles.add(role3);
-					await i.reply({ content: 'Role added', ephemeral:true });
-				} else {
-					member.roles.remove(role3);
-					await i.reply({ content: 'Role removed', ephemeral:true });
-				}
-			}else if (i.customId === 'button4') {
-				if (!member.roles.cache.has(role4.id)) {
-					member.roles.add(role4);
-					await i.reply({ content: 'Role added', ephemeral:true });
-				} else {
-					member.roles.remove(role4);
-					await i.reply({ content: 'Role removed', ephemeral:true });
-				}
-			}else if (i.customId === 'button5') {
-				if (!member.roles.cache.has(role5.id)) {
-					member.roles.add(role5);
-					await i.reply({ content: 'Role added', ephemeral:true });
-				} else {
-					member.roles.remove(role5);
-					await i.reply({ content: 'Role removed', ephemeral:true });
-				}
-			}
-			else {
-				await i.reply({ content: 'nothing happened (this is an error)', ephemeral:true });
-			}
 			await wait(4000);
 			await i.deleteReply();
+			} else {
+				await i.reply({ content: 'nothing happened (this is an error)', ephemeral:true });
+    			return;
+			}
 		});
 	},
 
