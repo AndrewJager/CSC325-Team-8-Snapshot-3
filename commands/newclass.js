a5015c28a5984131b1c7d4cd1cd787d92b24aef9
shunt4 <sphunt@umich.edu>
2023-03-22 19:20:47 -0400

    Ignore 'Students' role

--- commands/newclass.js
+++ commands/newclass.js
@@ -59,7 +59,7 @@ module.exports = {
             rolesList.sort(function(a, b) {
                 return b.position - a.position;
             });
-            const studentRoles = rolesList.filter(item => item.name.includes('Students'));
+            const studentRoles = rolesList.filter(item => /\d\d\d Students/.test(item.name));
             // Create student role, if it doesn't already exist
             if (!interaction.guild.roles.cache.find(role => role.name == studentsRole)) {
                 studentColor = await database.getAvailableColor();
